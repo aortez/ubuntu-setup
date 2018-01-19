01dff500659ad3ffa8cf6ce15119ffa91a99d810
Allan Ortiz <aortez@gmail.com>
2018-01-18 20:36:47 -0800

    finally get this keymapping correct.  Previous I was having problems with the Home key still functioning as an Alt modifier, at the same time as functioning as a Home key, thus having undesired side-effects

--- set_home_end_keys.sh
+++ set_home_end_keys.sh
@@ -1,11 +1,14 @@
 #/bin/bash
 # My Dell Latitude 7480 does not have physical Home and End keys (they require a Fn modifier),
 # this script fixes that by remapping the Alt and Ctrl keys to Home and End.
-
+# I used the following references:
+# https://unix.stackexchange.com/questions/65507/use-setxkbmap-to-swap-the-left-shift-and-left-control/65600#65600
+# https://unix.stackexchange.com/a/414668
 
 if grep --quiet add_home_and_end_keys$ ~/.bashrc; then
 	echo "it is already done!"
 else
+	echo "mapping right alt and control to Home and End"
 	# copy some files into home dir
 	cp -r .xkb ~/
 
