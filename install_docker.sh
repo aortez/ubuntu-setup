a4f758039e733da028a5fd297f7b52c30751129f
Allan Ortiz <aortez@gmail.com>
2018-03-09 16:01:24 -0800

    add command-line completion for compose

--- install_docker.sh
+++ install_docker.sh
@@ -37,3 +37,6 @@ set -e
 echo "install docker compose"
 sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+echo "enable command line completion for compose"
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.19.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
