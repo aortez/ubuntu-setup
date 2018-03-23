bc6ec03a79b19578f271654ab1809be8c3508869
Allan Ortiz <aortez@gmail.com>
2018-03-22 17:32:02 -0700

    hopefully fix rvm install script

--- install_ruby.sh
+++ install_ruby.sh
@@ -6,6 +6,10 @@ echo "installing rvm..."
 sudo apt-add-repository -y ppa:rael-gc/rvm
 sudo apt-get update
 sudo apt-get install -y rvm
+pushd .
+cd /usr/local/bin
+sudo ln -s /usr/share/rvm/bin/rvm
+popd
 
 echo "logout, login, then run:"
 echo "rvm install ruby"
