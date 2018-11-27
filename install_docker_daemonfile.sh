a327009ed54d164ff27a55dd0e37cb7b31b41dda
Allan Ortiz <aortez@gmail.com>
2018-11-27 13:08:50 -0800

    add notes about fixing dns on docker via daemon.json

---
+++ install_docker_daemonfile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eu
+echo "just notes for now, please read"
+exit 1
+
+nmcli dev show | grep 'IP4.DNS'
+IP4.DNS[1]:              10.0.0.2
+IP4.DNS[2]:              10.0.0.3
+
+use the DNS IPs to create a file
+$ cat /etc/docker/daemon.json
+{
+    "dns": ["208.67.220.220", "208.67.222.222"]
+}
+sudo service docker restart
