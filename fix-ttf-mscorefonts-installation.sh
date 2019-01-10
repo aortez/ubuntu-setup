53ddfa4e8d41127bcebca9cc3d9fea65cd4827d2
Allan Ortiz <aortez@gmail.com>
2019-01-10 10:37:25 -0800

    are the ttf-mscorefonts fixed?  maybe!

--- fix-ttf-mscorefonts-installation.sh
+++ fix-ttf-mscorefonts-installation.sh
@@ -9,3 +9,6 @@ sudo apt install ./ttf-mscorefonts-installer_3.7_all.deb
 #echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 sudo apt install ttf-mscorefonts-installer
 rm $FILENAME
+
+# force update of font cache - not %100 sure this is the final missing piece, but we will see
+sudo fc-cache -f -v
