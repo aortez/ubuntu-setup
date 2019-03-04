c11b1396c81ac75af3d8ae96c23b2c2acffc14d0
Allan Ortiz <aortez@gmail.com>
2019-03-04 13:35:27 -0800

    Re-add steps to move docker data dir to the data partition.
    This was accidentally removed when switching from PPA docker to default
    repo docker. Also re-organized a bit so that all docker stuff is
    prefixed like 'docker_<do something>'.

---
+++ docker_move_data_off_root.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# By default docker's data directory is placed on the root partition.
+# My preference is to keep large data files off the root partition and instead
+# store them in a data partition (this way the OS can be reinstalled without
+# messing with your data).
+# So let's store all those docker images and whatnot on the data partition.
+set -euo pipefail
+
+echo "stopping docker service"
+sudo service docker stop
+
+echo "moving existing docker data to data dir"
+sudo mkdir -p /home/data
+sudo mv /var/lib/docker/ /home/data/
+
+echo "symlinking old docker data dir to new data dir"
+sudo ln -s -T /home/data/docker/ /var/lib/docker
+
+echo "restarting docker service"
+sudo service docker start
