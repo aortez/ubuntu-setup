d5ed334060a45b2fbc0739d4d27d6efa3ec36cbc
Allan Ortiz <allan.ortiz@flukenetworks.com>
2021-02-16 10:48:08 -0800

    use mako to fix notification dismissal in swaywm

--- install_virtualbox.sh
+++ install_virtualbox.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 set -eouxf pipefail
 
+# this guide was pretty useful:
+# https://www.techrepublic.com/article/how-to-enable-usb-in-virtualbox/
+
 # add key
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 
 # add repo
 sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
+
+# install USB extension
+# download from here:
+# https://www.virtualbox.org/wiki/Downloads
+# Install via File -> Preferences -> Extensions -> add
+
+# Add user to vboxusers group
+# sudo usermod -aG vboxusers <USERNAME>
+
+# install guest additions...
+# Here is an example version - modify for whatever the version you are using is
+https://download.virtualbox.org/virtualbox/6.1.14/VBoxGuestAdditions_6.1.14.iso
