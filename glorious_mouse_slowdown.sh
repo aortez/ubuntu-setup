ad4661bb95f429be352c97439bdbb8f9d7b5fd78
Allan Ortiz <allan.ortiz@flukenetworks.com>
2021-03-14 12:52:06 -0700

    add script to lower mouse sensitivity
    This lowers it below what you can do via the UI.

---
+++ glorious_mouse_slowdown.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+# Lowers the mouse sensitivity to half the minimum level shown in the UI.
+# This is useful if you run DPIs higher than 400.
+xinput --set-prop "Glorious Model D" "Coordinate Transformation Matrix" 0.5 0 0 0 0.5 0 0 0 2
