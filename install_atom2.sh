9d4ea9464dea788a04a3395d2c5fb65b901695b0
Allan Ortiz <aortez@gmail.com>
2021-11-15 16:02:25 -0800

    Add a style script for Atom to control the UI font scale

--- install_atom2.sh
+++ install_atom2.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 echo "Configuring Atom and installing packages"
 
 echo "Installing custom keymap..."
-# gets rid of the SHIFT-UP/DOWN to move lines shortcut
-# I am not used to that semantic and it always happens unintentionally
+# Get rid of the SHIFT-UP/DOWN to move lines shortcut.
+# I am not used to that semantic and it always happens unintentionally.
 set -x
 set +e
 mv ~/.atom/keymap.cson ~/.atom/keymap.bak
@@ -13,11 +13,12 @@ set -e
 cp atom_keymap.cson ~/.atom/keymap.cson
 
 set +x
+echo "Installing custom style..."
+set -x
+cp atom_styles.less ~/.atom/styles.less
+
+set +x
 echo "Installing packages..."
 set -x
 apm install atom-ide-ui
-apm install docblockr
-apm install ide-typescript
-apm install atom-ide-ui
-apm install linter-eslint
 apm install plantuml-preview; sudo apt install plantuml
