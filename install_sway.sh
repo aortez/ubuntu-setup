787ea75f2bfb9f62c8e02f4a096fd9629103cc15
Allan Ortiz <aortez@gmail.com>
2023-11-22 14:42:48 -0800

    update sway install
    This is probably close to being a complete installation procedure now.

--- install_sway.sh
+++ install_sway.sh
@@ -1,7 +1,17 @@
-# Just the start of notes for installing swaywm, might take a while before this
-# is solid.
+#!/bin/bash
+# installs sway and packages
+set -eux
 
-need:
-xclip
+sudo apt install grim \
+	sway \
+	mako-notifier \
+	waybar \
+	wl-clipboard \
+	xclip
 
-mako-notifier
+echo 'export MOZ_ENABLE_WAYLAND=1' | sudo tee /etc/profile.d/firefox
+
+# Something is broken for apparmor and Mako, so let's just disable it for the moment.
+# https://github.com/emersion/mako/issues/257
+sudo apt install apparmor-utils
+sudo aa-disable /etc/apparmor.d/fr.emersion.Mako
