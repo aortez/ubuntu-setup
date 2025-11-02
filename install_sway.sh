8a01f3f04a513f23e35d60abe2f29e4205d352a3
Allan Ortiz <aortez@gmail.com>
2025-11-02 15:41:47 -0800

    add mako config, copy sway configs to system

--- install_sway.sh
+++ install_sway.sh
@@ -18,3 +18,13 @@ echo 'export MOZ_ENABLE_WAYLAND=1' | sudo tee /etc/profile.d/firefox
 # https://github.com/emersion/mako/issues/257
 sudo apt install apparmor-utils
 sudo aa-disable /etc/apparmor.d/fr.emersion.Mako
+
+# Copy configuration files.
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+mkdir -p ~/.config/sway
+mkdir -p ~/.config/waybar
+mkdir -p ~/.config/mako
+cp "$SCRIPT_DIR/sway_config" ~/.config/sway/config
+cp "$SCRIPT_DIR/waybar_config" ~/.config/waybar/config
+cp "$SCRIPT_DIR/waybar_style.css" ~/.config/waybar/style.css
+cp "$SCRIPT_DIR/mako_config" ~/.config/mako/config
